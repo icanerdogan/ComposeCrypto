a5c39efd0d410b4d0d365c8b1822dde1e92dae27
Ibrahim.Erdogan <ibrahim.erdogan@logo.com.tr>
2022-12-21 21:51:38 +0300

    navigation compose #1

--- app/src/main/java/com/ibrahimcanerdogan/composecrypto/MainActivity.kt
+++ app/src/main/java/com/ibrahimcanerdogan/composecrypto/MainActivity.kt
@@ -3,38 +3,46 @@ package com.ibrahimcanerdogan.composecrypto
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.remember
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.ibrahimcanerdogan.composecrypto.ui.theme.ComposeCryptoTheme
+import com.ibrahimcanerdogan.composecrypto.view.CryptoDetailScreen
+import com.ibrahimcanerdogan.composecrypto.view.CryptoListScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeCryptoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                val navController = rememberNavController()
+
+                NavHost(navController = navController, startDestination = "crypto_list_screen") {
+                    composable("crypto_list_screen") {
+                        CryptoListScreen(navController)
+                    }
+                    // yanyana gönderme sırası yazılmaktadır.
+                    composable("crypto_detail_screen/{cryptoId}/{cryptoPrice}", arguments = listOf(
+                        navArgument("cryptoId") {
+                            type = NavType.StringType
+                        },
+                        navArgument("cryptoPrice") {
+                            type = NavType.StringType
+                        }
+                    )) {
+                        val cryptoId = remember {
+                            it.arguments?.getString("cryptoId")
+                        }
+                        val cryptoPrice = remember {
+                            it.arguments?.getString("cryptoPrice")
+                        }
+                        CryptoDetailScreen(cryptoId ?: "", cryptoPrice ?: "", navController)
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeCryptoTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/ibrahimcanerdogan/composecrypto/view/CryptoListScreen.kt
@@ -0,0 +1,10 @@
+package com.ibrahimcanerdogan.composecrypto.view
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun CryptoListScreen(navController: NavController) {
+    Text("Hello World!")
+}
